314b44ccf6070c34fdf2bb7ac4375c6f0cccc750
diptadeepray <diptadeepray@gmail.com>
2025-03-16 13:28:04 +0530

    BuildGradle_Changed_ToEnable_ViewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.readrover"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding= true
+    }
+
     defaultConfig {
         applicationId = "com.example.readrover"
         minSdk = 24
